9dfd3ca005d8cd4d06ddf07e7885d6606c9a1006
1849179-angular-basico <josue211523@gmail.com>
2022-11-26 18:05:27 -0600

    moviles

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IonicModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
